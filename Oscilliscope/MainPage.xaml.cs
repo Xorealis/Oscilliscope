821e99d136121955501ddda68d4a8ae263f6930f
Chase V <cv1438695@gmail.com>
2023-11-29 21:11:06 -0800

    Implemented some databinding variables and ported Serial logic to Viewmodel

--- Oscilliscope/MainPage.xaml.cs
+++ Oscilliscope/MainPage.xaml.cs
@@ -5,66 +5,10 @@ namespace Oscilliscope
 {
     public partial class MainPage : ContentPage
     {
-        private bool bPortOpen = false;
-        private string newPacket = "";
-        private string sendPacket = "";
-        const int samples = 32;
-        private float[] voltages = new float[samples];
-
-        SerialPort serialPort = new SerialPort();
 
         public MainPage()
         {
             InitializeComponent();
-            string[] ports = SerialPort.GetPortNames();
-            portPicker.ItemsSource = ports;
-            portPicker.SelectedIndex = ports.Length;
-            Loaded += MainPage_Loaded;
-        }
-
-        private void MainPage_Loaded(object sender, EventArgs e)
-        {
-            serialPort.BaudRate = 115200;
-            serialPort.ReceivedBytesThreshold = 1;
-            serialPort.DataReceived += SerialPort_DataRecieved;
-        }
-
-        private void SerialPort_DataRecieved(object sender, EventArgs e)
-        {
-            newPacket = serialPort.ReadLine();
-            MainThread.BeginInvokeOnMainThread(MyMainCodeThread);
-        }
-
-        private void MyMainCodeThread()
-        {
-            if(newPacket.Substring(0, 3) == "###")
-            {
-                int currPlace = 0;
-                for(int i = 6; i < (samples * 4); i += 4)
-                {
-                    voltages[currPlace] = Int32.Parse(newPacket.Substring(i, 4)) / 4096.0f;
-                    currPlace++;
-                }
-            }
-        }
-
-        private void BtnOpenClose_Clicked(object sender, EventArgs e)
-        {
-            if (!bPortOpen)
-            {
-                //Opens selected port, changes button action to "Close"
-                serialPort.PortName = portPicker.SelectedItem.ToString();
-                serialPort.Open();
-                BtnOpenClose.Text = "Close";
-                bPortOpen = true;
-            }
-            else
-            {
-                //Closes selected port, changes button action to "Open"
-                serialPort.Close();
-                BtnOpenClose.Text = "Open";
-                bPortOpen = false;
-            }
         }
     }
 }
--- Oscilliscope/ViewModel.cs
+++ Oscilliscope/ViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO.Ports;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using LiveChartsCore;
@@ -10,19 +11,73 @@ using LiveChartsCore.SkiaSharpView;
 
 namespace Oscilliscope;
 
-public partial class ViewModel : ObservableObject
+[ObservableObject]
+public partial class ViewModel
 {
     private static ObservableCollection<ObservableValue> observableValues;
+    private bool bPortOpen = false;
+    private string newPacket = "";
+    private string sendPacket = "";
+    const int samples = 32;
+    private float[] voltages = new float[samples];
+
+    [ObservableProperty]
+    public string comPortButtonText = "Open";
+
+    [ObservableProperty]
+    public string[] comPorts;
+
+    [ObservableProperty]
+    public string selectedPort = "";
+
+    SerialPort serialPort = new SerialPort();
 
     public ViewModel()
     {
-        observableValues = new ObservableCollection<ObservableValue>
-        {
-        new ObservableValue(2),
-        new ObservableValue(3),
-        new ObservableValue(4),
-        };
+        serialPort.BaudRate = 115200;
+        serialPort.ReceivedBytesThreshold = 1;
+        serialPort.DataReceived += SerialPort_DataReceived;
+
+        observableValues = new ObservableCollection<ObservableValue> { };
+        string[] ports = SerialPort.GetPortNames();
+        comPorts = ports;
     }
+
+    private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        newPacket = serialPort.ReadLine();
+
+        if (newPacket.Substring(0, 3) == "###")
+        {
+            int currPlace = 0;
+            for (int i = 6; i < (samples * 4); i += 4)
+            {
+                voltages[currPlace] = Int32.Parse(newPacket.Substring(i, 4)) / 4096.0f;
+                currPlace++;
+            }
+        }
+    }
+
+    [RelayCommand]
+    void OpenClose()
+    {
+        if (!bPortOpen)
+        {
+            //Opens selected port, changes button action to "Close"
+            serialPort.PortName = SelectedPort;
+            serialPort.Open();
+            comPortButtonText = "Close";
+            bPortOpen = true;
+        }
+        else
+        {
+            //Closes selected port, changes button action to "Open"
+            serialPort.Close();
+            comPortButtonText = "Open";
+            bPortOpen = false;
+        }
+    }
+
 
     public ISeries[] Series { get; set; } =
     {
